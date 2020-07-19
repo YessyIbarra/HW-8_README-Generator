a16f6a2ed4f85e1af2b5926ebc54beba64ba4451
Yesenia Ibarra <yessyibarra17@gmail.com>
2020-07-18 23:28:31 -0700

    Adding more questions to get the PERFECT README.

--- starter_code/index.js
+++ starter_code/index.js
@@ -3,53 +3,63 @@
 
 // array of questions for user
 const questions = [
-{
-    type: "input",
-    name: "projectTitle",
-    message:"Enter Project Title"
-},
-{
-    type: "input",
-    name: "projectDescription",
-    message:"Enter Project Description"  
-},
-{
-    type: "input",
-    name: "install",
-    message:"Enter Project Installation Steps"  
-},
-{
-    type: "input",
-    name: "usage",
-    message:"Provide Examples of Project Usage"  
-},
-{
-    type: "list",
-    name: "license",
-    message:"Choose a License for Project",
-    choices: [{
-        name: "MIT License",
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "Enter Project Title"
+    },
+    {
+        type: "input",
+        name: "projectDescription",
+        message: "Enter Project Description"
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "Enter Project Installation Steps"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide Examples of Project Usage"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose a License for Project",
+        choices: [{
+            name: "MIT License",
         },
         {
-        name: "GPLv2 License",
+            name: "GPLv2 License",
         },
         {
-        name: "Apache License",
+            name: "Apache License",
         },
         {
-        name: "GPLv3 License"
+            name: "GPLv3 License"
         }]
     },
-{
-    type:"input",
-    name:"gitHub",
-    message:"Enter GitHub Username",
-},
-{
-    type:"input",
-    name:"email",
-    message:"Enter email address"
-}
+    {
+        type: "input",
+        name: "contributing",
+        message: "Specify Directions for Contributing to Your Project"
+    },
+    {
+        type: "input",
+        name: "testing",
+        message: "Specify Directions for Testing Your Project"
+    },
+    {
+        type: "input",
+        name: "gitHub",
+        message: "Enter GitHub Username",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter email address"
+    }
 
 ];
 
